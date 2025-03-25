7f1f66900c5ecb7796fda062254f7c5b6fea26d2
alternoegraha <noegrahachan@gmail.com>
2025-03-25 03:13:31 +0000

    touchscreen: ft8006s_spi: remove some stuff
    - remove delay_gesture func
    - remove g_regulator_status (unused)
    - ((ts_data->pm_suspend) && (ts_data->gesture_mode)) -> if ((ts_data->gesture_mode) && (ts_data->pm_suspend))
    - remove ts_data->fts_tp_class func (dunno what is this)
    - remove "touch reset gpio pull" code
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- drivers/input/touchscreen/ft8006s_spi/focaltech_core.c
+++ drivers/input/touchscreen/ft8006s_spi/focaltech_core.c
@@ -70,8 +70,6 @@ extern touchscreen_usb_plugin_data_t g_touchscreen_usb_pulgin;
 * Global variable or extern global variabls/functions
 *****************************************************************************/
 struct fts_ts_data *fts_data;
-static bool delay_gesture = false;
-static bool g_regulator_status;
 
 #if LCT_TP_USB_PLUGIN
 void fts_ts_usb_event_callback(void)
@@ -746,7 +744,7 @@ static irqreturn_t fts_irq_handler(int irq, void *data)
     int ret = 0;
     struct fts_ts_data *ts_data = fts_data;
 
-    if ((ts_data->pm_suspend) && (ts_data->gesture_mode)) {
+    if ((ts_data->gesture_mode) && (ts_data->pm_suspend)) {
         ret = wait_for_completion_timeout(
                   &ts_data->pm_completion,
                   msecs_to_jiffies(FTS_TIMEOUT_COMERR_PM));
@@ -1482,25 +1480,12 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
         FTS_ERROR("Unable to register drm_notifier: %d\n", ret);
     }
 
-	if (ts_data->fts_tp_class == NULL) {
-		if (ts_data->fts_tp_class) {
-			ts_data->fts_touch_dev = device_create(ts_data->fts_tp_class, NULL, 0x38, ts_data, "tp_dev");
-			if (IS_ERR(ts_data->fts_touch_dev)) {
-				FTS_ERROR("Failed to create device !");
-				goto err_class_create;
-			}
-			dev_set_drvdata(ts_data->fts_touch_dev, ts_data);
-		}
-	}
 #if LCT_TP_USB_PLUGIN
 	g_touchscreen_usb_pulgin.event_callback = fts_ts_usb_event_callback;
 #endif
 
     FTS_FUNC_EXIT();
     return 0;
-err_class_create:
-	class_destroy(ts_data->fts_tp_class);
-	ts_data->fts_tp_class = NULL;
 
 err_irq_req:
 #if FTS_POWER_SOURCE_CUST_EN
@@ -1616,8 +1601,6 @@ static int fts_ts_suspend(struct device *dev)
             }
 #endif
         }
-        /* touch reset gpio pull down */
-        gpio_direction_output(fts_data->pdata->reset_gpio, 0 );
     }
 
     fts_release_all_finger();
@@ -1635,10 +1618,6 @@ static int fts_ts_resume(struct device *dev)
         FTS_DEBUG("Already in awake state");
         return 0;
     }
-
-    /* if gesture_mode enabled, touch reset gpio pull up */
-    if (!ts_data->gesture_mode)
-        gpio_direction_output(fts_data->pdata->reset_gpio, 1 );
 
     fts_release_all_finger();
 
@@ -1662,10 +1641,6 @@ static int fts_ts_resume(struct device *dev)
     }
 
     ts_data->suspended = false;
-
-    if (delay_gesture) {
-        delay_gesture = false;
-    }
 
 	fts_irq_enable();
 
@@ -1712,7 +1687,6 @@ static int fts_ts_probe(struct spi_device *spi)
 {
     int ret = 0;
     struct fts_ts_data *ts_data = NULL;
-    delay_gesture = false;
 
     FTS_INFO("Touch Screen(SPI BUS) driver prboe...");
     spi->mode = SPI_MODE_0;
