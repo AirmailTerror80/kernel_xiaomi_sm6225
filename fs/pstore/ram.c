alternoegraha/krenol_ximi_fog

fs: pstore: Import xiaomi changes
• from spes-r-oss

Change-Id: 9a4b59b7bae0d2f47ef66c3e64498ded7f46c90b
Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

fs/pstore/ram.c
@@ -790,6 +790,9 @@ static int ramoops_probe(struct platform_device *pdev)
 
 	dump_mem_sz = cxt->size - cxt->console_size - cxt->ftrace_size
 			- cxt->pmsg_size;
+	pr_err("dump_mem_sz=%d, cxt->record_size=%d, cxt->size=%d, cxt->console_size=%d, cxt->ftrace_size=%d, cxt->pmsg_size=%d\n",
+			dump_mem_sz, cxt->record_size, cxt->size,
+			cxt->console_size, cxt->ftrace_size, cxt->pmsg_size);
 	err = ramoops_init_przs("dump", dev, cxt, &cxt->dprzs, &paddr,
 				dump_mem_sz, cxt->record_size,
 				&cxt->max_dump_cnt, 0, 0);

AirmailTerror80/kernel_xiaomi_sm6225

fs/pstore/ram.c
@@ -790,6 +790,9 @@ static int ramoops_probe(struct platform_device *pdev)
 
 	dump_mem_sz = cxt->size - cxt->console_size - cxt->ftrace_size
 			- cxt->pmsg_size;
+	pr_info("dump_mem_sz=%zu, cxt->record_size=%zu, cxt->size=%lu, cxt->console_size=%zu, cxt->ftrace_size=%zu, cxt->pmsg_size=%zu\n",
+		dump_mem_sz, cxt->record_size, cxt->size,
+		cxt->console_size, cxt->ftrace_size, cxt->pmsg_size);
 	err = ramoops_init_przs("dump", dev, cxt, &cxt->dprzs, &paddr,
 				dump_mem_sz, cxt->record_size,
 				&cxt->max_dump_cnt, 0, 0);