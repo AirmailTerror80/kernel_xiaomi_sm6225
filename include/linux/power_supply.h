mrsrimar22/android_kernel_qcom_sm8250

treewide: power_supply: Import xiaomi changes
The Patch based on QCOM release TAG:Snapdragon_Mid_2020.SPF.1.0.1R_r00024.0

From: spes-r-oss
Change-Id: Ia840bfd2e806c72b48ad426ff0b078214ec1625c
Signed-off-by: gongjingzhou <gongjingzhou@xiaomi.com>
Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

include/linux/power_supply.h
@@ -165,6 +165,9 @@ enum power_supply_property {
 	/* Properties of type `int' */
 	POWER_SUPPLY_PROP_STATUS = 0,
 	POWER_SUPPLY_PROP_CHARGE_TYPE,
+	/* add by xiaomi start */
+	POWER_SUPPLY_PROP_BATT_CHARGE_TYPE,
+	/* add by xiaomi end */
 	POWER_SUPPLY_PROP_HEALTH,
 	POWER_SUPPLY_PROP_PRESENT,
 	POWER_SUPPLY_PROP_ONLINE,
@@ -199,6 +202,26 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT,
 	POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX,
 	POWER_SUPPLY_PROP_INPUT_CURRENT_LIMIT,
+	/* add sc8551 node start */
+	POWER_SUPPLY_PROP_SC_BATTERY_PRESENT,
+	POWER_SUPPLY_PROP_SC_VBUS_PRESENT,
+	POWER_SUPPLY_PROP_SC_BATTERY_VOLTAGE,
+	POWER_SUPPLY_PROP_SC_BATTERY_CURRENT,
+	POWER_SUPPLY_PROP_SC_BATTERY_TEMPERATURE,
+	POWER_SUPPLY_PROP_SC_BUS_VOLTAGE,
+	POWER_SUPPLY_PROP_SC_BUS_CURRENT,
+	POWER_SUPPLY_PROP_SC_BUS_TEMPERATURE,
+	POWER_SUPPLY_PROP_SC_DIE_TEMPERATURE,
+	POWER_SUPPLY_PROP_SC_ALARM_STATUS,
+	POWER_SUPPLY_PROP_SC_FAULT_STATUS,
+	POWER_SUPPLY_PROP_SC_VBUS_ERROR_STATUS,
+	/* add sc8551 node for check chip vendor, because maxim IIC address have conflict with sc8551 */
+	POWER_SUPPLY_PROP_SC_CHIP_VENDOR,
+	POWER_SUPPLY_PROP_OTG_ENABLE,
+	POWER_SUPPLY_PROP_SC_REG_STATUS,
+	POWER_SUPPLY_PROP_SC_SET_BUS_PROTECTION_FOR_QC3,
+	// POWER_SUPPLY_PROP_OTG_ENABLE,
+	/* add sc8551 node end */
 	POWER_SUPPLY_PROP_ENERGY_FULL_DESIGN,
 	POWER_SUPPLY_PROP_ENERGY_EMPTY_DESIGN,
 	POWER_SUPPLY_PROP_ENERGY_FULL,
@@ -209,6 +232,14 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CAPACITY_ALERT_MIN, /* in percents! */
 	POWER_SUPPLY_PROP_CAPACITY_ALERT_MAX, /* in percents! */
 	POWER_SUPPLY_PROP_CAPACITY_LEVEL,
+	/* add sm5602_fg node start */
+	POWER_SUPPLY_PROP_SHUTDOWN_DELAY,
+	POWER_SUPPLY_PROP_SOC_DECIMAL,
+	POWER_SUPPLY_PROP_SOC_DECIMAL_RATE,
+	POWER_SUPPLY_PROP_COLD_THERMAL_LEVEL,
+	POWER_SUPPLY_PROP_FFC_TERMINATION_CURRENT,
+	POWER_SUPPLY_PROP_RECHARGE_VBAT,
+	/* add sm5602_fg node end */
 	POWER_SUPPLY_PROP_TEMP,
 	POWER_SUPPLY_PROP_TEMP_MAX,
 	POWER_SUPPLY_PROP_TEMP_MIN,
@@ -350,6 +381,13 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_VOLTAGE_STEP,
 	POWER_SUPPLY_PROP_APSD_RERUN,
 	POWER_SUPPLY_PROP_APSD_TIMEOUT,
+	/* refine of maxim, so resume this */
+	POWER_SUPPLY_PROP_FASTCHARGE_MODE,
+	POWER_SUPPLY_PROP_CHIP_OK,
+	POWER_SUPPLY_PROP_TERMINATION_CURRENT,
+	POWER_SUPPLY_PROP_MTBF_CUR,
+	/* refine of maxim end */
+
 	/* Charge pump properties */
 	POWER_SUPPLY_PROP_CP_STATUS1,
 	POWER_SUPPLY_PROP_CP_STATUS2,
@@ -366,6 +404,26 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CC_TOGGLE_ENABLE,
 	POWER_SUPPLY_PROP_FG_TYPE,
 	POWER_SUPPLY_PROP_CHARGER_STATUS,
+	/* add by xiaomi start */
+#ifdef CONFIG_BATT_VERIFY_BY_DS28E16
+	/* battery verify properties */
+	POWER_SUPPLY_PROP_ROMID,
+	POWER_SUPPLY_PROP_DS_STATUS,
+	POWER_SUPPLY_PROP_PAGENUMBER,
+	POWER_SUPPLY_PROP_PAGEDATA,
+	POWER_SUPPLY_PROP_AUTHEN_RESULT,
+	POWER_SUPPLY_PROP_SESSION_SEED,
+	POWER_SUPPLY_PROP_S_SECRET,
+	POWER_SUPPLY_PROP_CHALLENGE,
+	POWER_SUPPLY_PROP_AUTH_ANON,
+	POWER_SUPPLY_PROP_AUTH_BDCONST,
+	POWER_SUPPLY_PROP_PAGE0_DATA,
+	POWER_SUPPLY_PROP_PAGE1_DATA,
+	POWER_SUPPLY_PROP_VERIFY_MODEL_NAME,
+#endif
+	POWER_SUPPLY_PROP_QUICK_CHARGE_TYPE,
+	POWER_SUPPLY_PROP_TERM_CURRENT,
+	/* add by xiaomi end */
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
@@ -405,6 +463,10 @@ enum power_supply_type {
 	POWER_SUPPLY_TYPE_UFP,			/* Type-C UFP */
 	POWER_SUPPLY_TYPE_DFP,			/* Type-C DFP */
 	POWER_SUPPLY_TYPE_CHARGE_PUMP,		/* Charge Pump */
+	/* add battery verify properties */
+#ifdef CONFIG_BATT_VERIFY_BY_DS28E16
+	POWER_SUPPLY_TYPE_BATT_VERIFY,		/* battery verify */
+#endif
 };
 
 enum power_supply_usb_type {
@@ -455,10 +517,25 @@ enum power_supply_notifier_events {
 	PSY_EVENT_PROP_CHANGED,
 };
 
+/* add for nopmi charger */
+#if defined(CONFIG_NOPMI_CHARGER)
+typedef enum _NOPMI_CHARGER_IC_TYPE {
+	NOPMI_CHARGER_IC_NONE,
+	NOPMI_CHARGER_IC_MAXIM,
+	NOPMI_CHARGER_IC_SYV,
+	NOPMI_CHARGER_IC_SC,
+	NOPMI_CHARGER_IC_MAX
+} NOPMI_CHARGER_IC_TYPE;
+#endif
+
 union power_supply_propval {
 	int intval;
 	const char *strval;
 	int64_t int64val;
+	/* add battery verify properties */
+#ifdef CONFIG_BATT_VERIFY_BY_DS28E16
+	unsigned char arrayval[50];
+#endif
 };
 
 struct device_node;

AirmailTerror80/kernel_xiaomi_sm6225

include/linux/power_supply.h
@@ -165,6 +165,9 @@ enum power_supply_property {
 	/* Properties of type `int' */
 	POWER_SUPPLY_PROP_STATUS = 0,
 	POWER_SUPPLY_PROP_CHARGE_TYPE,
+	/* add by xiaomi start */
+	POWER_SUPPLY_PROP_BATT_CHARGE_TYPE,
+	/* add by xiaomi end */
 	POWER_SUPPLY_PROP_HEALTH,
 	POWER_SUPPLY_PROP_PRESENT,
 	POWER_SUPPLY_PROP_ONLINE,
@@ -199,6 +202,26 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT,
 	POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX,
 	POWER_SUPPLY_PROP_INPUT_CURRENT_LIMIT,
+	/* add sc8551 node start */
+	POWER_SUPPLY_PROP_SC_BATTERY_PRESENT,
+	POWER_SUPPLY_PROP_SC_VBUS_PRESENT,
+	POWER_SUPPLY_PROP_SC_BATTERY_VOLTAGE,
+	POWER_SUPPLY_PROP_SC_BATTERY_CURRENT,
+	POWER_SUPPLY_PROP_SC_BATTERY_TEMPERATURE,
+	POWER_SUPPLY_PROP_SC_BUS_VOLTAGE,
+	POWER_SUPPLY_PROP_SC_BUS_CURRENT,
+	POWER_SUPPLY_PROP_SC_BUS_TEMPERATURE,
+	POWER_SUPPLY_PROP_SC_DIE_TEMPERATURE,
+	POWER_SUPPLY_PROP_SC_ALARM_STATUS,
+	POWER_SUPPLY_PROP_SC_FAULT_STATUS,
+	POWER_SUPPLY_PROP_SC_VBUS_ERROR_STATUS,
+	/* add sc8551 node for check chip vendor, because maxim IIC address have conflict with sc8551 */
+	POWER_SUPPLY_PROP_SC_CHIP_VENDOR,
+	POWER_SUPPLY_PROP_OTG_ENABLE,
+	POWER_SUPPLY_PROP_SC_REG_STATUS,
+	POWER_SUPPLY_PROP_SC_SET_BUS_PROTECTION_FOR_QC3,
+	// POWER_SUPPLY_PROP_OTG_ENABLE,
+	/* add sc8551 node end */
 	POWER_SUPPLY_PROP_ENERGY_FULL_DESIGN,
 	POWER_SUPPLY_PROP_ENERGY_EMPTY_DESIGN,
 	POWER_SUPPLY_PROP_ENERGY_FULL,
@@ -209,6 +232,14 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CAPACITY_ALERT_MIN, /* in percents! */
 	POWER_SUPPLY_PROP_CAPACITY_ALERT_MAX, /* in percents! */
 	POWER_SUPPLY_PROP_CAPACITY_LEVEL,
+	/* add sm5602_fg node start */
+	POWER_SUPPLY_PROP_SHUTDOWN_DELAY,
+	POWER_SUPPLY_PROP_SOC_DECIMAL,
+	POWER_SUPPLY_PROP_SOC_DECIMAL_RATE,
+	POWER_SUPPLY_PROP_COLD_THERMAL_LEVEL,
+	POWER_SUPPLY_PROP_FFC_TERMINATION_CURRENT,
+	POWER_SUPPLY_PROP_RECHARGE_VBAT,
+	/* add sm5602_fg node end */
 	POWER_SUPPLY_PROP_TEMP,
 	POWER_SUPPLY_PROP_TEMP_MAX,
 	POWER_SUPPLY_PROP_TEMP_MIN,
@@ -350,6 +381,13 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_VOLTAGE_STEP,
 	POWER_SUPPLY_PROP_APSD_RERUN,
 	POWER_SUPPLY_PROP_APSD_TIMEOUT,
+	/* refine of maxim, so resume this */
+	POWER_SUPPLY_PROP_FASTCHARGE_MODE,
+	POWER_SUPPLY_PROP_CHIP_OK,
+	POWER_SUPPLY_PROP_TERMINATION_CURRENT,
+	POWER_SUPPLY_PROP_MTBF_CUR,
+	/* refine of maxim end */
+
 	/* Charge pump properties */
 	POWER_SUPPLY_PROP_CP_STATUS1,
 	POWER_SUPPLY_PROP_CP_STATUS2,
@@ -366,6 +404,26 @@ enum power_supply_property {
 	POWER_SUPPLY_PROP_CC_TOGGLE_ENABLE,
 	POWER_SUPPLY_PROP_FG_TYPE,
 	POWER_SUPPLY_PROP_CHARGER_STATUS,
+	/* add by xiaomi start */
+#ifdef CONFIG_BATT_VERIFY_BY_DS28E16
+	/* battery verify properties */
+	POWER_SUPPLY_PROP_ROMID,
+	POWER_SUPPLY_PROP_DS_STATUS,
+	POWER_SUPPLY_PROP_PAGENUMBER,
+	POWER_SUPPLY_PROP_PAGEDATA,
+	POWER_SUPPLY_PROP_AUTHEN_RESULT,
+	POWER_SUPPLY_PROP_SESSION_SEED,
+	POWER_SUPPLY_PROP_S_SECRET,
+	POWER_SUPPLY_PROP_CHALLENGE,
+	POWER_SUPPLY_PROP_AUTH_ANON,
+	POWER_SUPPLY_PROP_AUTH_BDCONST,
+	POWER_SUPPLY_PROP_PAGE0_DATA,
+	POWER_SUPPLY_PROP_PAGE1_DATA,
+	POWER_SUPPLY_PROP_VERIFY_MODEL_NAME,
+#endif
+	POWER_SUPPLY_PROP_QUICK_CHARGE_TYPE,
+	POWER_SUPPLY_PROP_TERM_CURRENT,
+	/* add by xiaomi end */
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_PROP_CHARGE_COUNTER_EXT,
 	/* Properties of type `const char *' */
@@ -383,6 +441,10 @@ enum power_supply_property {
 enum power_supply_type {
 	POWER_SUPPLY_TYPE_UNKNOWN = 0,
 	POWER_SUPPLY_TYPE_BATTERY,
+	/* add battery verify properties */
+#ifdef CONFIG_BATT_VERIFY_BY_DS28E16
+	POWER_SUPPLY_TYPE_BATTERY_VERIFY,	/* battery verify */
+#endif
 	POWER_SUPPLY_TYPE_UPS,
 	POWER_SUPPLY_TYPE_MAINS,
 	POWER_SUPPLY_TYPE_USB,			/* Standard Downstream Port */
@@ -455,10 +517,25 @@ enum power_supply_notifier_events {
 	PSY_EVENT_PROP_CHANGED,
 };
 
+/* add for nopmi charger */
+#if defined(CONFIG_NOPMI_CHARGER)
+typedef enum _NOPMI_CHARGER_IC_TYPE {
+	NOPMI_CHARGER_IC_NONE,
+	NOPMI_CHARGER_IC_MAXIM,
+	NOPMI_CHARGER_IC_SYV,
+	NOPMI_CHARGER_IC_SC,
+	NOPMI_CHARGER_IC_MAX
+} NOPMI_CHARGER_IC_TYPE;
+#endif
+
 union power_supply_propval {
 	int intval;
 	const char *strval;
 	int64_t int64val;
+	/* add battery verify properties */
+#ifdef CONFIG_BATT_VERIFY_BY_DS28E16
+	unsigned char arrayval[50];
+#endif
 };
 
 struct device_node;